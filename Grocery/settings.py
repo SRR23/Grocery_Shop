28ec119f495314298b85d14cf0896bc85c456356
SRR23 <srrimon267@gmail.com>
2024-08-03 22:26:11 +0600

    added

--- Grocery/settings.py
+++ Grocery/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [BASE_DIR / 'assets']
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
